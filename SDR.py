c37f0f0520e26a33b44ba2ef9d515335c30cd9c9
jazielmax <davidmaurtua1@gmail.com>
2026-03-05 16:40:34 -0500

    more notes + loop

--- SDR.py
+++ SDR.py
@@ -1,22 +1,33 @@
 import numpy as np
 from rtlsdr import RtlSdr
 from scipy.signal import decimate, firwin, lfilter
+import time
 
 import sounddevice as sd
 
+#already get a 2.4mhz bandwith (it just gets cut off later in demodulation)
+#
+for i in range(3):
 #Configuration
-sdr = RtlSdr()          #create SDR object
-sdr.sample_rate = 2.4e6      #sample per second
-sdr.center_freq = 100.7e6    #radio tuning
-sdr.gain = 'auto'           #higher = if signal stronger
-                        #lower if signal is weaker
-
-print("Receiving samples...")
-samples = sdr.read_samples(2_400_000 * 10)  #2.4e6 samples read / 2.4e6 samples rate = 1 second
-sdr.close()
+    sdr = RtlSdr()          #create SDR object
+    sdr.sample_rate = 2.4e6      #sample per second
+    sdr.center_freq = 100.7e6    #radio tuning
+    sdr.gain = 'auto'           #higher = if signal stronger
+                            #lower if signal is weaker
 
 
-print("Signal power:", np.mean(np.abs(samples)))
+    freq_increase = sdr.center_freq + 200_000
+    print("Receiving samples...")
+    start_time = time.time()
+    samples = sdr.read_samples(2_400_000 * 0.25)  #2.4e6 samples read / 2.4e6 samples rate = 1 second
+    sdr.close()
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    print(elapsed_time)
+
+
+    print("Signal power:", np.mean(np.abs(samples)))
 
 # --- FM Demodulation ---
 nyq = sdr.sample_rate / 2
@@ -42,3 +53,16 @@ sd.wait()
 
 
 #look into rtl_power (sweeps data over wide frequency spectrum)
+#not useful for pipeline but it can detect active frequencies (can scan for strong signals in dB over frequency range)
+#can automatically discover stations in a new area
+
+#fingerprinting
+#audio -> time vs frequency (currently in amplitude v time) use short time fourier transform
+#find STRONG frequency peaks (you'll have coordinate points of peaks)
+#pair peaks together (don't store individual peaks, pair them together)
+#hash those pairs
+#store hashes in database (non relational database is KING (sqlite for local storage though))
+
+#bad news have to build own hash database
+#so essentially we have to create the hashes and store them
+#which means we need LEGALLY OBTAIN music and create a script to hash them and store it into our cute database

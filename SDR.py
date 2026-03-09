ed733c2879829433ce0210aeee153f9414dcbe0e
DaPoms <DanielTripolitan@gmail.com>
2026-03-09 19:42:56 -0400

    Playing around with libraries

--- SDR.py
+++ SDR.py
@@ -4,41 +4,50 @@ from scipy.signal import decimate, firwin, lfilter
 
 import sounddevice as sd
 
-#Configuration
-sdr = RtlSdr()          #create SDR object
-sdr.sample_rate = 2.4e6      #sample per second
-sdr.center_freq = 100.7e6    #radio tuning
-sdr.gain = 'auto'           #higher = if signal stronger
-                        #lower if signal is weaker
 
-print("Receiving samples...")
-samples = sdr.read_samples(2_400_000 * 10)  #2.4e6 samples read / 2.4e6 samples rate = 1 second
+
+# reading is by default 2.4 MHZ
+
+#Reading
+
+sdr = RtlSdr()          # create SDR object
+sdr.sample_rate = 2.4e6      # sample per second
+
+sdr.gain = 'auto'           # higher = if signal weaker
+                        # lower if signal is stronger
+
+for i in range(0,1):
+    print("Receiving samples...")
+    sdr.center_freq = 105.3e6 + (i * 0.2e6)    # radio tuning
+    samples = sdr.read_samples(sdr.sample_rate * 2 )  #2.4e6 samples read / 2.4e6 samples rate = 1 second
+    #Interpretation
+    print("Signal power:", np.mean(np.abs(samples)))
+
+    # --- FM Demodulation ---
+    nyq = sdr.sample_rate / 2
+    cutoff = 200e3 # cutoff is related to bandwidth
+    taps = firwin(101, cutoff / nyq) #filter length is 101, cutoff is 200khz (how FM bandwith works), normalized
+ 
+    filtered = lfilter(taps, 1.0, samples) #applies filter 
+    print("Demodulating FM...")
+    phase_diff = np.angle(filtered[1:] * np.conj(filtered[:-1]))    #actually demodulates (data stored in frequency)
+
+    # --- Downsample to audio rate ---
+    audio = decimate(phase_diff, 10)  # 2.4 MHz → 240 kHz
+    audio = decimate(audio, 5)        # 240 kHz → 48 kHz        (48khz )
+
+    # Normalize
+    audio /= np.max(np.abs(audio))          #scales between -1 and 1 (volume reasons)
+
+    print("Playing audio...")
+    sd.play(audio, 48000)
+    sd.wait()
+frequencyDom = np.fft.fft(samples)
+
+print(phase_diff.max())
+
 sdr.close()
-
-
-print("Signal power:", np.mean(np.abs(samples)))
-
-# --- FM Demodulation ---
-nyq = sdr.sample_rate / 2
-cutoff = 200e3
-taps = firwin(101, cutoff / nyq) #filter length is 101, cutoff is 200khz (how FM bandwith works), normalized
-
-filtered = lfilter(taps, 1.0, samples) #applies filter 
-print("Demodulating FM...")
-phase_diff = np.angle(filtered[1:] * np.conj(filtered[:-1]))    #actually demodulates (data stored in frequency)
-
-# --- Downsample to audio rate ---
-audio = decimate(phase_diff, 10)  # 2.4 MHz → 240 kHz
-audio = decimate(audio, 5)        # 240 kHz → 48 kHz        (48khz )
-
-# Normalize
-audio /= np.max(np.abs(audio))          #scales between -1 and 1 (volume reasons)
-
-print("Playing audio...")
-sd.play(audio, 48000)
-sd.wait()
-
-#"C:\Users\Jazz\AppData\Local\Programs\Python\Python314\Lib\site-packages\rtlsdr\__init__.py"
-
-
 #look into rtl_power (sweeps data over wide frequency spectrum)
+
+
+
